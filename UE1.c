e121f2327d72ca3d95261a3f1b3c39d25ccfb8a1
Noremo83 <kolanandreas@gmx.at>
2018-10-20 06:02:04 -0700

    asdf

--- UE1.c
+++ UE1.c
@@ -1,6 +1,28 @@
 #include<stdio.h>
+#include<time.h>
+#include<unistd.h>
+#include "mytime.h"
 
 int main(){
 
-
+	struct timespec start;
+	struct timespec stop;
+	struct timespec diff;
+	struct timespec ges;
+	ges.tv_sec = 0;
+	ges.tv_nsec = 0;
+	
+	start = get_cur_time_161314();
+	write_time_161314("StartZeit: ",&start);
+	sleep(5);
+	stop = get_cur_time_161314();	
+	write_time_161314("StopZeit: ",&stop);
+	
+	diff = get_diff_161314(&start,&stop);
+	write_time_161314("DiffZeit: ",&diff);
+	
+	add_time_161314(&ges,&diff);
+	write_time_161314("geszeit: ",&ges);
+	
+	printf("\n\n");
 }
