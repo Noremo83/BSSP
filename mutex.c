fac82244fad193789410bb57999754273c3b74a8
Noremo83 <kolanandreas@gmx.at>
2018-10-21 02:55:01 -0700

    For loop theads und Fehlerkontrolle

--- mutex.c
+++ mutex.c
@@ -8,41 +8,54 @@
 #include<openssl/md5.h>
 #include<sys/stat.h>
 
-#define MAXTHR 2
+#define MIN_REQUIRED 2
+
 pthread_mutex_t dirsync = PTHREAD_MUTEX_INITIALIZER;
 
-void *work( void * ptr);
+void *work_161314( void * ptr);
 
 int main(int argc, char **argv){
+	int i = 0;
+	long maxthread = 0;
 
-	DIR *dirp;
-	//int maxthread = 2; // maxthread ist der zweite Parameter argv[2]
-	pthread_t threadID[MAXTHR];
+	//Kontrolle ob genug Argumente angegeben wurden
+	if (argc <= MIN_REQUIRED){
+		fprintf(stderr,"To less arguments.\nUSAGE:\n    PATH(/etc/....)\nTHRADCOUNT(1-10)\n");
+		exit(1);
+	}
+
+	maxthread = atoi(argv[2]);
+	//Kontrolle ob die Anzahl der Threads zwischen 1 und 10 liegen.
+	if (atoi(argv[2]) > 0 && atoi(argv[2]) <= 10){
+		maxthread = atoi(argv[2]);
+	}
 	
-	//argv[1]
+	printf("maxthread: %ld\n",maxthread);
+	DIR *dirp;
+	pthread_t threadID[maxthread];
 	dirp = opendir(argv[1]);
 	
-	if (!dirp){ // if = 0 dann fehler konnte nicht geöffnet werden
-		fprintf(stderr,"VZ open geht net\n");
+	// if = 0 dann fehler konnte nicht geöffnet werden
+	if (!dirp){
+		fprintf(stderr,"Could not open Directory!\n");
 		exit(1);
 	}
 	
-	pthread_create(threadID,NULL,work,(void *)(dirp));
-	pthread_create(threadID+1,NULL,work,(void *)(dirp));
-	pthread_create(threadID+2,NULL,work,(void *)(dirp));
+	for(i=0;i <= maxthread;i++){
+		pthread_create(threadID + i,NULL,work_161314,(void *)(dirp));
+	}
 	
-	pthread_join(threadID[0],NULL); // NULL ist der Rückgabewert des Threads kann auch ein &ptr sein
-	pthread_join(threadID[1],NULL);
-	pthread_join(threadID[2],NULL);
-	
+	for(i=0;i<=maxthread;i++){
+		pthread_join(threadID[i],NULL);
+	}
+
 	return 0;
 	
 }
 
-void *work(void * ptr){
+void *work_161314(void * ptr){
 	static int nr = 0;
 	int mynr = ++nr;
-	//printf("%c",mynr);
 	
 	struct dirent * entryp;
 	
